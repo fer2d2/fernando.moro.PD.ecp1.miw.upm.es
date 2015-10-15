97d8033e80a065e7b19d10f42fa7079cb8e1ee16
Fernando Moro <fernando.moro@radmas.com>
2015-10-15 21:29:23 +0200

    Added initial tests stub

---
+++ src/test/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberTest.java
@@ -0,0 +1,46 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class NaturalNumberTest {
+    
+    NaturalNumberManager naturalNumberManager;
+    
+    @Before
+    public void before() {
+        this.naturalNumberManager = new NaturalNumberManager();
+    }
+
+    @Test
+    public void testNaturalNumberCreatorES() {
+    
+    }
+    
+    @Test
+    public void testNaturalNumberCreatorESOutOfBounds() {
+    
+    }
+    
+    @Test
+    public void testNaturalNumberCreatorEN() {
+
+    }
+
+    @Test
+    public void testNaturalNumberCreatorENOutOfBounds() {
+    
+    }
+    
+    @Test
+    public void testNaturalNumberCreatorFR() {
+
+    }
+
+    @Test
+    public void testNaturalNumberCreatorFROutOfBounds() {
+    
+    }
+    
+}
