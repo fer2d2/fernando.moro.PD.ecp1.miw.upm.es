d42d4ae99488cb0a568d3ed4f8b0e941ca2f91e6
Fernando Moro <fernando.moro@radmas.com>
2015-10-15 20:45:07 +0200

    Added class EstadoEsperando

---
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoEsperando.java
@@ -0,0 +1,56 @@
+package es.upm.miw.pd.state.connection;
+
+public class EstadoEsperando extends EstadoConexion {
+
+    public EstadoEsperando(Conexion conexion) {
+        super(conexion);
+    }
+
+    @Override
+    public void abrir() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void cerrar() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void iniciar() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void parar() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void recibir(int response) {
+        EstadoConexion estado;
+        
+        if(response == 0) {
+            estado = new EstadoPreparado(this.conexion);
+        } else {
+            estado = new EstadoCerrado(this.conexion);
+        }
+        
+        this.conexion.setEstado(estado);
+    }
+
+    @Override
+    public void enviar(String request) {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public Estado getEstado() {
+        return Estado.ESPERANDO;
+    }
+}
