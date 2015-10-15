4e46d083207d27927f18ca715aa58ddf9abc8d38
Fernando Moro <fernando.moro@radmas.com>
2015-10-15 20:42:19 +0200

    Added abstract class EstadoConexion

---
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoConexion.java
@@ -0,0 +1,29 @@
+package es.upm.miw.pd.state.connection;
+
+public abstract class EstadoConexion {
+
+    protected Conexion conexion;
+
+    public EstadoConexion(Conexion conexion) {
+        super();
+        this.conexion = conexion;
+    }
+    
+    public abstract void abrir();
+
+    public abstract void cerrar();
+    
+    public abstract void iniciar();
+
+    public abstract void parar();
+    
+    public abstract void recibir(int response);
+    
+    public abstract void enviar(String request);
+
+    public Conexion getConexion() {
+        return conexion;
+    }
+    
+    public abstract Estado getEstado();
+}
