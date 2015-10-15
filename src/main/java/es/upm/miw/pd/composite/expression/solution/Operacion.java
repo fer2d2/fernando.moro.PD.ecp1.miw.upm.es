a0caca6ccff257956c6f95d91a68ce0c9909f2fd
Fernando Moro <fernando.moro@radmas.com>
2015-10-15 19:55:34 +0200

    Added toString to Operacion

--- src/main/java/es/upm/miw/pd/composite/expression/solution/Operacion.java
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Operacion.java
@@ -1,6 +1,7 @@
 package es.upm.miw.pd.composite.expression.solution;
 
-abstract class Operacion extends Expresion {
+public abstract class Operacion extends Expresion {
+    
     Expresion elemento1;
 
     Expresion elemento2;
@@ -9,4 +10,6 @@ abstract class Operacion extends Expresion {
         this.elemento1 = elemento1;
         this.elemento2 = elemento2;
     }
+    
+    public abstract String toString();
 }
