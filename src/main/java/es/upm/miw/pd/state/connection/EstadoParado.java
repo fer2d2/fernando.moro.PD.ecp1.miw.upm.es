ee5c19b00e1c4249af61023ded08851b9e2abb1a
Fernando Moro <fernando.moro@radmas.com>
2015-10-15 20:44:49 +0200

    Added class EstadoParado

---
+++ src/main/java/es/upm/miw/pd/state/connection/EstadoParado.java
@@ -0,0 +1,47 @@
+package es.upm.miw.pd.state.connection;
+
+public class EstadoParado extends EstadoConexion {
+
+    public EstadoParado(Conexion conexion) {
+        super(conexion);
+    }
+
+    @Override
+    public void abrir() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void cerrar() {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void iniciar() {
+        this.conexion.setEstado(new EstadoPreparado(this.conexion));
+        
+    }
+
+    @Override
+    public void parar() {
+    }
+
+    @Override
+    public void recibir(int response) {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public void enviar(String request) {
+        throw new UnsupportedOperationException("Acción no permitida... ");
+        
+    }
+
+    @Override
+    public Estado getEstado() {
+        return Estado.PARADO;
+    }
+}
