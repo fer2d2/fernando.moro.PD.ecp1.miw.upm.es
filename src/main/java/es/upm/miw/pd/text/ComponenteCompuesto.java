f8d8e7cd0cb1a2c13c592b150dee530c28948725
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 13:12:01 +0200

    Added support for ComponenteCompuesto

---
+++ src/main/java/es/upm/miw/pd/text/ComponenteCompuesto.java
@@ -0,0 +1,30 @@
+package es.upm.miw.pd.text;
+
+import java.util.ArrayList;
+
+public abstract class ComponenteCompuesto extends Componente {
+
+    protected ArrayList<Componente> componentes = new ArrayList<Componente>();
+
+    @Override
+    public Boolean isCompuesto() {
+        return true;
+    }
+    
+    @Override
+    public String dibujar(boolean uppercase) {
+        String dibujo = obtenerDibujoDeComponentes(uppercase);
+        return dibujo;
+    }
+
+    private String obtenerDibujoDeComponentes(boolean uppercase) {
+        String dibujo = "";
+        
+        for (Componente componente : componentes) {
+            dibujo += componente.dibujar(uppercase);
+        }
+        
+        return dibujo;
+    }
+
+}
