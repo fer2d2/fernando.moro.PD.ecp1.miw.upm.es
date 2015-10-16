80d1413c15446f6fd58145942637391bc3a7703a
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 11:48:20 +0200

    Added natural number manager

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberManager.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberManager.java
@@ -2,4 +2,19 @@ package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public class NaturalNumberManager {
 
+    private NaturalNumberCreator naturalNumberCreator;
+    
+    private NaturalNumber naturalNumber;
+
+    public void setNaturalNumberCreator(NaturalNumberCreator naturalNumberCreator) {
+        this.naturalNumberCreator = naturalNumberCreator;
+    }
+
+    public void createNaturalNumber(int number) {
+        this.naturalNumber = this.naturalNumberCreator.createNaturalNumber(number);
+    }
+
+    public NaturalNumber getNaturalNumber() {
+        return naturalNumber;
+    }
 }
