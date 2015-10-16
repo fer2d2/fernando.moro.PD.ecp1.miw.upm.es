a605cc2229befdd9d885aa4841fe86198a35797a
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 11:47:24 +0200

    Updated NaturalNumberEs

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEs.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEs.java
@@ -1,27 +1,14 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber;
 
-public class NaturalNumberEs {
-    private int value;
-
+public class NaturalNumberEs extends NaturalNumber {
+    
     private static final String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
 
     public NaturalNumberEs(int value) {
-        this.setValue(value);
+        super(value);
     }
 
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        assert value >= 0;
-        this.value = value;
-    }
-
-    public void add(int value) {
-        this.setValue(this.value + value);
-    }
-
+    @Override
     public String getTextValue() {
         if (this.value < textValue.length) {
             return NaturalNumberEs.textValue[this.value];
@@ -29,5 +16,4 @@ public class NaturalNumberEs {
             return "???";
         }
     }
-
 }
