4212a93ab63682063076cfc5a72c086bb78885f5
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 12:39:12 +0200

    Added implementation for Caracter

--- src/main/java/es/upm/miw/pd/text/Caracter.java
+++ src/main/java/es/upm/miw/pd/text/Caracter.java
@@ -2,4 +2,30 @@ package es.upm.miw.pd.text;
 
 public class Caracter extends Componente {
 
+    private char caracter;
+    
+    public Caracter(char caracter) {
+        this.caracter = caracter;
+    }
+
+    @Override
+    public void add(Componente componente) {
+    }
+
+    @Override
+    public String dibujar(Boolean uppercase) {
+        String dibujo = String.valueOf(this.caracter);
+        
+        if(uppercase) {
+            return dibujo.toUpperCase();
+        }
+        
+        return dibujo;
+    }
+
+    @Override
+    public Boolean isCompuesto() {
+        return false;
+    }
+
 }
