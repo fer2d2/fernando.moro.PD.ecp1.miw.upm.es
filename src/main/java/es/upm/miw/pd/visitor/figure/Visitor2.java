87d9c0ca634af509d39178d5f97e2ef199875faf
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 12:13:58 +0200

    Added visitor2 class to count number of visits

---
+++ src/main/java/es/upm/miw/pd/visitor/figure/Visitor2.java
@@ -0,0 +1,27 @@
+package es.upm.miw.pd.visitor.figure;
+
+public class Visitor2 extends Visitor {
+    
+    private int numberOfAs = 0;
+    
+    private int numberOfBs = 0;
+    
+    @Override
+    public void visitElementA(ElementA e) {
+        this.numberOfAs++;
+    }
+
+    @Override
+    public void visitElementB(ElementB e) {
+        this.numberOfBs++;
+    }
+
+    public int getAs() {
+        return numberOfAs;
+    }
+
+    public int getBs() {
+        return numberOfBs;
+    }
+
+}
