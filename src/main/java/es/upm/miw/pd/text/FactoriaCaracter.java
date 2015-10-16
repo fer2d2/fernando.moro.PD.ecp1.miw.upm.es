c7fde464644d1c523cab57a3113e38b9df3aa136
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 13:19:17 +0200

    Updated class FactoriaCaracter

--- src/main/java/es/upm/miw/pd/text/FactoriaCaracter.java
+++ src/main/java/es/upm/miw/pd/text/FactoriaCaracter.java
@@ -1,13 +1,37 @@
 package es.upm.miw.pd.text;
 
+import java.util.HashMap;
+
 public class FactoriaCaracter {
 
+    private static FactoriaCaracter factoriaCaracter;
+    
+    private HashMap<Character, Caracter> caracteres;
+    
+    private FactoriaCaracter() {
+        this.caracteres = new HashMap<Character, Caracter>();
+    }
+
     public static FactoriaCaracter getFactoria() {
-        return null;
+        if(FactoriaCaracter.factoriaCaracter == null) {
+            FactoriaCaracter.factoriaCaracter = new FactoriaCaracter(); 
+        }
+        
+        return FactoriaCaracter.factoriaCaracter;
     }
 
     public Caracter get(char c) {
-        return null;
+        Caracter caracter = caracteres.get(new Character(c));
+        caracter = addCaracterToCaracteresIfNotPresent(c, caracter);
+        return caracter;
+    }
+
+    private Caracter addCaracterToCaracteresIfNotPresent(char c, Caracter caracter) {
+        if(caracter == null){
+            caracter = new Caracter(Character.valueOf(c));
+            caracteres.put(Character.valueOf(c), caracter);
+        }
+        return caracter;
     }
 
 }
