93a03fce8300d9c61b9dc694be1ef11edb74aec8
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 13:12:48 +0200

    Texto is now a ComponenteCompuesto

--- src/main/java/es/upm/miw/pd/text/Texto.java
+++ src/main/java/es/upm/miw/pd/text/Texto.java
@@ -1,5 +1,23 @@
 package es.upm.miw.pd.text;
 
-public class Texto extends Componente {
+public class Texto extends ComponenteCompuesto {
 
+    private final String separador = "---o---\n";
+    
+    @Override
+    public void add(Componente componente) {
+        
+        if(!componente.isCompuesto()) {
+            throw new UnsupportedOperationException("Solo se deberian introducir elementos compuestos");
+        }
+        
+        this.componentes.add(componente);
+    }
+
+    @Override
+    public String dibujar(boolean uppercase) {
+        String resultado = super.dibujar(uppercase);
+        resultado += this.separador;
+        return resultado;
+    }
 }
