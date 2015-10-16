094b33649ea2074babea295035bc4c5bc36f129c
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 11:48:42 +0200

    Added tests for natural number factory-method pattern

--- src/test/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberTest.java
+++ src/test/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberTest.java
@@ -15,32 +15,50 @@ public class NaturalNumberTest {
 
     @Test
     public void testNaturalNumberCreatorES() {
-    
+        this.naturalNumberManager.setNaturalNumberCreator(new NaturalNumberEsCreator());
+        this.naturalNumberManager.createNaturalNumber(1);
+        NaturalNumber naturalNumber = this.naturalNumberManager.getNaturalNumber();
+        assertEquals("uno", naturalNumber.getTextValue());
     }
     
     @Test
     public void testNaturalNumberCreatorESOutOfBounds() {
-    
+        this.naturalNumberManager.setNaturalNumberCreator(new NaturalNumberEsCreator());
+        this.naturalNumberManager.createNaturalNumber(6);
+        NaturalNumber naturalNumber = this.naturalNumberManager.getNaturalNumber();
+        assertEquals("???", naturalNumber.getTextValue());
     }
     
     @Test
     public void testNaturalNumberCreatorEN() {
-
+        this.naturalNumberManager.setNaturalNumberCreator(new NaturalNumberEnCreator());
+        this.naturalNumberManager.createNaturalNumber(2);
+        NaturalNumber naturalNumber = this.naturalNumberManager.getNaturalNumber();
+        assertEquals("two", naturalNumber.getTextValue());
     }
 
     @Test
     public void testNaturalNumberCreatorENOutOfBounds() {
-    
+        this.naturalNumberManager.setNaturalNumberCreator(new NaturalNumberEnCreator());
+        this.naturalNumberManager.createNaturalNumber(6);
+        NaturalNumber naturalNumber = this.naturalNumberManager.getNaturalNumber();
+        assertEquals("???", naturalNumber.getTextValue());
     }
     
     @Test
     public void testNaturalNumberCreatorFR() {
-
+        this.naturalNumberManager.setNaturalNumberCreator(new NaturalNumberFrCreator());
+        this.naturalNumberManager.createNaturalNumber(5);
+        NaturalNumber naturalNumber = this.naturalNumberManager.getNaturalNumber();
+        assertEquals("cinq", naturalNumber.getTextValue());
     }
 
     @Test
     public void testNaturalNumberCreatorFROutOfBounds() {
-    
+        this.naturalNumberManager.setNaturalNumberCreator(new NaturalNumberFrCreator());
+        this.naturalNumberManager.createNaturalNumber(6);
+        NaturalNumber naturalNumber = this.naturalNumberManager.getNaturalNumber();
+        assertEquals("???", naturalNumber.getTextValue());
     }
     
 }
