235d4400dc82932a78def67789a4516641d20232
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 13:09:43 +0200

    Added class Componente

--- src/main/java/es/upm/miw/pd/text/Componente.java
+++ src/main/java/es/upm/miw/pd/text/Componente.java
@@ -4,7 +4,7 @@ public abstract class Componente {
 
     public abstract void add(Componente componente);
     
-    public abstract String dibujar(Boolean uppercase);
+    public abstract String dibujar(boolean uppercase);
     
     public abstract Boolean isCompuesto();
 
