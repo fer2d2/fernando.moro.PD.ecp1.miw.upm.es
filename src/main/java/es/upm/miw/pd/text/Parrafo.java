94b07176d09a81d4949b5da6245ded52287316c2
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 13:12:29 +0200

    Parrafo is now a ComponenteCompuesto

--- src/main/java/es/upm/miw/pd/text/Parrafo.java
+++ src/main/java/es/upm/miw/pd/text/Parrafo.java
@@ -1,5 +1,24 @@
 package es.upm.miw.pd.text;
 
-public class Parrafo extends Componente {
+public class Parrafo extends ComponenteCompuesto {
+    
+    private final String separador = "\n";
+    
+    @Override
+    public void add(Componente componente) {
+        
+        if(componente.isCompuesto()) {
+            throw new UnsupportedOperationException("No se pueden introducir elementos compuestos");
+        }
+        
+        this.componentes.add(componente);
+    }
+
+    @Override
+    public String dibujar(boolean uppercase) {
+        String resultado = super.dibujar(uppercase);
+        resultado += this.separador;
+        return resultado;
+    }
 
 }
