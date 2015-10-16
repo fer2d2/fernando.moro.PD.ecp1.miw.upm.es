a95895f5cac1a2fa4636ecf71b9956f0ec773c9b
Fernando Moro <fernando.moro@radmas.com>
2015-10-16 12:27:45 +0200

    Added stub classes inferred from the test class

---
+++ src/main/java/es/upm/miw/pd/text/Componente.java
@@ -0,0 +1,13 @@
+package es.upm.miw.pd.text;
+
+public abstract class Componente {
+
+    public void add(Componente h) {
+        
+    }
+
+    public String dibujar(boolean b) {
+        return null;
+    }
+
+}
---
+++ src/main/java/es/upm/miw/pd/text/FactoriaCaracter.java
@@ -0,0 +1,13 @@
+package es.upm.miw.pd.text;
+
+public class FactoriaCaracter {
+
+    public static FactoriaCaracter getFactoria() {
+        return null;
+    }
+
+    public Caracter get(char c) {
+        return null;
+    }
+
+}
