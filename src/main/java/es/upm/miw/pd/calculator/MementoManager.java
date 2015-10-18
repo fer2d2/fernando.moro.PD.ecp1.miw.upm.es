074c1a039a80a88aaad0a305ec89b7cf36a4ceac
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 21:21:19 +0200

    Added manager for mementos

---
+++ src/main/java/es/upm/miw/pd/calculator/MementoManager.java
@@ -0,0 +1,24 @@
+package es.upm.miw.pd.calculator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MementoManager<T> {
+    private Map<String, T> backups;
+
+    public MementoManager() {
+        this.backups = new HashMap<String, T>();
+    }
+    
+    public void addMemento(String key, T memento) {
+        this.backups.put(key, memento);
+    }
+    
+    public T getMemento(String key){
+        return this.backups.get(key);
+    }
+    
+    public String[] keys() {
+        return (String[]) this.backups.keySet().toArray();
+    }
+}
