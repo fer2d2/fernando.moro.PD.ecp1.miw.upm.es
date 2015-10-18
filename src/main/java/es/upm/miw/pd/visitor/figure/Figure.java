d67698354e995ef5375ac2e4e4eb8fe922085316
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 19:31:08 +0200

    Moved Figure to interface as the pattern specification says

--- src/main/java/es/upm/miw/pd/visitor/figure/Figure.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/Figure.java
@@ -1,9 +1,6 @@
 package es.upm.miw.pd.visitor.figure;
 
-public abstract class Figure {
-
-    public Figure(String description) {
-    }
+public interface Figure {
 
     public abstract double accept(Visitor v);
 
