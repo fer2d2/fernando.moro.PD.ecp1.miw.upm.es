7fae2ac70f28ace039ff46a9c05206cd4fcea22b
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 21:09:32 +0200

    Added memento to maintain a memory of the values in the calculator

---
+++ src/main/java/es/upm/miw/pd/calculator/Memento.java
@@ -0,0 +1,19 @@
+package es.upm.miw.pd.calculator;
+
+public class Memento {
+    private int quantity;
+    
+    public Memento(int quantity) {
+        super();
+        this.quantity = quantity;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
