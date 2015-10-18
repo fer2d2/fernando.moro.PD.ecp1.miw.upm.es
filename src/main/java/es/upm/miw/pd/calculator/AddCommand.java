7a95864b4832bc92ba85d803aef63d3588579125
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 20:35:15 +0200

    Added AddCommand

---
+++ src/main/java/es/upm/miw/pd/calculator/AddCommand.java
@@ -0,0 +1,22 @@
+package es.upm.miw.pd.calculator;
+
+import upm.jbb.IO;
+
+public class AddCommand extends ComandoGenerico {
+
+    public AddCommand(Calculator calculator) {
+        super(calculator);
+    }
+
+    @Override
+    public String name() {
+        return "Add";
+    }
+
+    @Override
+    public void execute() {
+        int valor = IO.getIO().readInt();
+        this.calculator.add(valor);
+    }
+
+}
