772b6dcc242a735525486fed2d9d0b17e95d37ca
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 18:53:27 +0200

    Updated interface Visitor

--- src/main/java/es/upm/miw/pd/visitor/figure/Visitor.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/Visitor.java
@@ -2,16 +2,10 @@ package es.upm.miw.pd.visitor.figure;
 
 public interface Visitor {
     
-    public abstract void visitSquareArea(Square square);
-
-    public abstract void visitSquareNumberOfSides(Square square);
+    public abstract double visitSquare(Square square);
     
-    public abstract void visitTriangleArea(Triangle triangle);
-
-    public abstract void visitTriangleNumberOfSides(Triangle triangle);
+    public abstract double visitTriangle(Triangle triangle);
     
-    public abstract void visitCircleArea(Circle circle);
-
-    public abstract void visitCircleNumberOfSides(Circle circle);
+    public abstract double visitCircle(Circle circle);
     
 }
