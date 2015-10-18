0849acf12ce2830cd9ebdef6f38fa3d38255f7dc
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 18:56:41 +0200

    Added Visitor for Number of sides

---
+++ src/main/java/es/upm/miw/pd/visitor/figure/VisitorNumberOfSides.java
@@ -0,0 +1,17 @@
+package es.upm.miw.pd.visitor.figure;
+
+public class VisitorNumberOfSides {
+
+    public double visitSquare(Square square) {
+        return 4;
+    }
+    
+    public double visitTriangle(Triangle triangle) {
+        return 3;
+    }
+    
+    public double visitCircle(Circle circle) {
+        return Double.POSITIVE_INFINITY;
+    }
+    
+}
