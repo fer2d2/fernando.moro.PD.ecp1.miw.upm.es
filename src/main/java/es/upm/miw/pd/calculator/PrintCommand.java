f6945c77affd53bcf928818de1fef5cc01ce4aa1
Fernando Moro <fernando.moro@radmas.com>
2015-10-18 20:39:47 +0200

    Added PrintCommand

---
+++ src/main/java/es/upm/miw/pd/calculator/PrintCommand.java
@@ -0,0 +1,20 @@
+package es.upm.miw.pd.calculator;
+
+import upm.jbb.IO;
+
+public class PrintCommand extends ComandoGenerico {
+    public PrintCommand(Calculator calculator) {
+        super(calculator);
+    }
+
+    @Override
+    public String name() {
+        return "Print";
+    }
+
+    @Override
+    public void execute() {
+        int valor = this.calculator.getTotal();
+        IO.getIO().println(valor);
+    }
+}
